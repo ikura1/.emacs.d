a211c4addb7b965103c18eef39ff4d1e5810ee9b
奥田侑紀 <ikura.lab@gmail.com>
2017-04-30 04:15:51 +0900

    rm trash

--- lisp のコピー/config/20-pyenv.el
+++
@@ -1,5 +0,0 @@
-;; pyenv package. url: https://github.com/cyberved/pyenv.el
-(require 'pyenv)
-(setq pyenv-show-active-python-in-modeline nil)
-(setq pyenv-modeline-function 'pyenv--modeline-plain)
-(global-pyenv-mode)
--- lisp のコピー/config/25-lisp.el
+++
@@ -1,4 +0,0 @@
-(setq inferior-lisp-program "sbcl")
-
-(require 'slime)
-(slime-setup '(slime-repl slime-fancy slime-banner))
