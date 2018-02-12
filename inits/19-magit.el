a4bbcf40d0254a3a3749b37b405aca34f866d402
ikura1 <ikura.lab@gmail.com>
2018-02-12 21:52:41 +0900

    fix: magitをlinuxに対応

--- inits/19-magit.el
+++ inits/19-magit.el
@@ -3,14 +3,16 @@
 ;;(setq magit-last-seen-setup-instructions "1.4.0")
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; windows用
-;(if (eq system-type 'windows-nt)
-;   (progn
-;(setq magit-git-executable "C:/Program Files/Git/cmd/git.exe")
-
-(setq magit-git-executable "C:/Program Files/Git/bin/git.exe")
-;(add-to-list 'exec-path "C:/Program Files/Git/bin")
-;))
+(cond ((eq system-type 'gnu/linux)
+      ;; Linux上のEmacs固有の設定
+          (setq magit-git-executable "/usr/bin/git"))
+      ;; Windows 7
+      ((eq system-type 'windows-nt)
+          (setq magit-git-executable "C:/Program Files/Git/cmd/git.exe"))
+      ;; Mac
+      ((eq system-type 'darwin)
+          (setq magit-git-executable ""))
+      )
 
 ;; 文字色変更
 (set-face-foreground 'magit-diff-added "#00FF00") ; 追加行
